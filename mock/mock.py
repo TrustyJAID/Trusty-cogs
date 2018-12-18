f538542ab2bae1b12ef944f552fa0817a5a04448
TrustyJAID <TrustyJAID@gmail.com>
2018-12-18 10:52:04 -0700

    [Mock] make more async and more pythonic

--- mock/mock.py
+++ mock/mock.py
@@ -1,6 +1,8 @@
 import discord
 import random
 from redbot.core import commands
+from typing import Union
+
 
 class Mock(getattr(commands, "Cog", object)):
     """mock a user as spongebob"""
@@ -8,7 +10,7 @@ class Mock(getattr(commands, "Cog", object)):
     def __init__(self, bot):
         self.bot = bot
 
-    def cap_change(self, message):
+    async def cap_change(self, message:str):
         result = ""
         for char in message:
             value = random.choice([True, False])
@@ -18,8 +20,8 @@ class Mock(getattr(commands, "Cog", object)):
                 result += char.lower()
         return result
 
-    @commands.command(pass_context=True)
-    async def mock(self, ctx, *, msg=""):
+    @commands.command()
+    async def mock(self, ctx, *, msg:Union[int, str]=None):
         """
             Mock a user with the spongebob meme
 
@@ -29,16 +31,22 @@ class Mock(getattr(commands, "Cog", object)):
         channel = ctx.message.channel
         result = ""
         user = ctx.message.author
-        if msg:
-            if msg.isdigit():
-                async for message in channel.history(limit=100):
-                    if str(message.id) == msg:
-                        msg = message
-                        break
-        else:
+        if type(msg) is int:
+
+            try:
+                msg = await ctx.channel.get_message(msg)
+            except:
+                return
+        elif msg is None:
             async for message in channel.history(limit=2):
                 msg = message
-        result = self.cap_change(msg.content) if hasattr(msg, "content") else self.cap_change(msg)
+        if type(msg) is discord.Message:
+            result = await self.cap_change(msg.content)
+            if result == "" and len(msg.embeds) != 0:
+                if msg.embeds[0].description != discord.Embed.Empty:
+                    result = await self.cap_change(msg.embeds[0].description)
+        else:
+            result = await self.cap_change(msg)
         author = msg.author if hasattr(msg, "author") else ctx.message.author
         time = msg.created_at if hasattr(msg, "created_at") else ctx.message.created_at
         embed = discord.Embed(description=result, timestamp=time)
@@ -46,7 +54,8 @@ class Mock(getattr(commands, "Cog", object)):
         embed.set_author(name=author.display_name, icon_url=author.avatar_url)
         embed.set_thumbnail(url="https://i.imgur.com/upItEiG.jpg")
         embed.set_footer(text="{} mocked {}".format(
-                         ctx.message.author.display_name, author.display_name), icon_url=ctx.message.author.avatar_url)
+                         ctx.message.author.display_name, author.display_name), 
+                        icon_url=ctx.message.author.avatar_url)
         if hasattr(msg, "attachments") and msg.attachments != []:
             embed.set_image(url=msg.attachments[0].url)
         if not channel.permissions_for(ctx.me).embed_links:
