750bd34ee2d0ff1559593c228aaa3cbf20988a19
TrustyJAID <TrustyJAID@gmail.com>
2019-01-03 21:34:49 -0700

    [TrustyAvatar] Allow generation of custom Avatars like TrustyBot

--- trustyavatar/trustyavatar.py
+++ trustyavatar/trustyavatar.py
@@ -1,10 +1,15 @@
 import discord
 import asyncio
 import aiohttp
+import functools
+import sys
 
 from redbot.core import commands, checks, Config
 from random import choice, randint
 from datetime import datetime
+from PIL import Image, ImageSequence
+from io import BytesIO
+from typing import Union, Optional
 
 
 
@@ -30,61 +35,186 @@ class TrustyAvatar(getattr(commands, "Cog", object)):
                         "game":["Please Remain Calm", "Mind the Gap"], 
                         "type":self.activities,
                         "link": "https://i.imgur.com/9iRBSeI.png",
-                        "xmas": "https://i.imgur.com/lSjMH5u.png"
+                        "xmas": "https://i.imgur.com/lSjMH5u.png",
+                        "transparent": "https://i.imgur.com/z1sHKYA.png"
                         },
                     "happy": {
                         "status":discord.Status.online,
                         "game":["Take it to Make it"],
                         "type":self.activities,
                         "link": "https://i.imgur.com/P5rUpET.png",
-                        "xmas": "https://i.imgur.com/zlQzAGP.png"
+                        "xmas": "https://i.imgur.com/zlQzAGP.png",
+                        "transparent": "https://i.imgur.com/b21iEMj.png"
                         },
                     "unamused": {
                         "status":discord.Status.idle, 
                         "game":["Obey Posted Limits"], 
                         "type":self.activities,
                         "link": "https://i.imgur.com/Wr3i9CG.png",
-                        "xmas": "https://i.imgur.com/Hp7XRjO.png"
+                        "xmas": "https://i.imgur.com/Hp7XRjO.png",
+                        "transparent": "https://i.imgur.com/cZoFosX.png"
                         },
                     "quizzical": {
                         "status": discord.Status.idle, 
                         "game":["Yellow Means Yield"], 
                         "type":self.activities,
                         "link": "https://i.imgur.com/qvBCgvU.png",
-                        "xmas": "https://i.imgur.com/1jCdG3x.png"
+                        "xmas": "https://i.imgur.com/1jCdG3x.png",
+                        "transparent": "https://i.imgur.com/3WBrM66.png"
                         },
                     "sad": {
                         "status": discord.Status.dnd, 
                         "game":["No Public Access"], 
                         "type":self.activities,
                         "link": "https://i.imgur.com/WwrZCTY.png",
-                        "xmas": "https://i.imgur.com/tb1FZjP.png"
+                        "xmas": "https://i.imgur.com/tb1FZjP.png",
+                        "transparent": "https://i.imgur.com/wPAdZ7S.png"
                         },
                     "angry": {
                         "status":discord.Status.dnd, 
                         "game":["Hitchhickers May Be Escaping Inmates"], 
                         "type":self.activities,
                         "link": "https://i.imgur.com/0JkYNGZ.png",
-                        "xmas": "https://i.imgur.com/hODosRi.png"
+                        "xmas": "https://i.imgur.com/hODosRi.png",
+                        "transparent": "https://i.imgur.com/L7PKqZF.png"
                         },
                     "watching": {
                         "status":discord.Status.dnd, 
                         "game":[" "], 
                         "type":[discord.ActivityType.watching],
                         "link": "https://i.imgur.com/Xs4Mwyd.png",
-                        "xmas": "https://i.imgur.com/xSLto00.png"
+                        "xmas": "https://i.imgur.com/xSLto00.png",
+                        "transparent": "https://i.imgur.com/X6SXXvx.png"
                         },
                    }
 
-    @commands.group(aliases=["ta"])
+    async def dl_image(self, url):
+        """Download bytes like object of user avatar"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                test = await resp.read()
+                return BytesIO(test)
+
+    def replace_colour(self, img, to_colour):
+        """https://stackoverflow.com/questions/765736/using-pil-to-make-all-white-pixels-transparent"""
+        img = Image.open(img)
+        img = img.convert("RGBA")
+        datas = img.getdata()
+        reds =[94, 221, 170]
+        greens = [123, 227, 185]
+        blues = [75, 217, 160]
+        newData = []
+        for item in datas:
+            # if item[0] in [94] and item[1] in [123] and item[2] in [75]:
+            if item[3] == 0:
+                newData.append(to_colour)
+            else:
+                newData.append(item)
+
+        img.putdata(newData)
+        temp = BytesIO()
+        img.save(temp, format="PNG")
+        temp.name = ("trustyavatar.png")
+        temp.seek(0)
+        return temp
+
+    def make_new_avatar(self, author_avatar, choice_avatar, is_gif):
+        avatar = Image.open(author_avatar)
+        new_avatar = Image.open(choice_avatar)
+        if is_gif:
+            gif_list = [frame.copy() for frame in ImageSequence.Iterator(avatar)]
+            img_list = []
+            for frame in gif_list:
+                temp2 = frame.copy()
+                w,h = temp2.size
+                new_avatar = new_avatar.resize((w,h), Image.ANTIALIAS)
+                temp2.paste(new_avatar, (0,0), new_avatar)
+                temp2 = temp2.resize((200,200), Image.ANTIALIAS)
+                img_list.append(temp2)
+                temp = BytesIO()
+                temp2.save(temp, format="GIF", save_all=True, append_images=img_list, duration=0, loop=0)
+                temp.name = "trustyavatar.gif"
+                if sys.getsizeof(temp) > 7000000 and sys.getsizeof(temp) < 8000000:
+                    break                
+        else:
+            temp2 = avatar.copy()
+            w,h = temp2.size
+            new_avatar = new_avatar.resize((w,h))
+            temp2.paste(new_avatar, (0,0), new_avatar)
+            temp2 = temp2.resize((200,200), Image.ANTIALIAS)
+            temp = BytesIO()
+            temp2.save(temp, format="PNG")
+            temp.name = "trustyavatar.png"
+        if temp:
+            temp.seek(0)
+            return temp
+
+
+    @commands.command(aliases=["ta"])
+    async def trustyavatar(self, 
+                           ctx, 
+                           style:Union[discord.Member, discord.Colour]=None,
+                           face:Optional[str]=None,
+                           is_gif:bool=False
+                           ):
+        """
+            Create your own avatar like TrustyBot's
+            
+            `style` can be a user or a colour code if none is supplied the authors avatar
+            is used
+            `face` must be one of neutral, happy, unamused, quizzical,
+            sad, angry, or watching if none are supplied a random one is picked
+            
+        """
+        author = ctx.author
+        new_avatar = choice([s for s in self.statuses])
+        if face:
+            if face.lower() not in self.statuses:
+                await ctx.send("That is not a valid choice.")
+                return
+            new_avatar = face
+        if type(style) is discord.Colour:
+            choice_avatar = await self.dl_image(self.statuses[new_avatar]["transparent"])
+            task = functools.partial(self.replace_colour,
+                                     img = choice_avatar,
+                                     to_colour=style.to_rgb())
+            task = self.bot.loop.run_in_executor(None, task)
+            try:
+                file = await asyncio.wait_for(task, timeout=60)
+            except asyncio.TimeoutError:
+                return
+        else:
+            if type(style) is discord.Member:
+                author = style
+            if author.is_avatar_animated() and is_gif:
+                author_avatar = await self.dl_image(author.avatar_url_as(format="gif"))
+            else:
+                author_avatar = await self.dl_image(author.avatar_url_as(format="png"))
+            choice_avatar = await self.dl_image(self.statuses[new_avatar]["transparent"])
+            task = functools.partial(self.make_new_avatar,
+                                     author_avatar = author_avatar,
+                                     choice_avatar = choice_avatar,
+                                     is_gif=is_gif)
+            task = self.bot.loop.run_in_executor(None, task)
+            try:
+                file = await asyncio.wait_for(task, timeout=60)
+            except asyncio.TimeoutError:
+                return
+        
+        image = discord.File(file)
+        await ctx.send(file=image)
+
+
+
+    @commands.group(aliases=["taset"])
     @checks.is_owner()
-    async def trustyavatar(self, ctx):
+    async def trustyavatarset(self, ctx):
         """
             Commands for overriding aspects of the bots avatar changes
         """
         pass
 
-    @trustyavatar.command()
+    @trustyavatarset.command()
     async def set(self, ctx, *, name:str):
         """
             Manually change preset options
@@ -100,7 +230,7 @@ class TrustyAvatar(getattr(commands, "Cog", object)):
             await self.change_avatar(url)
         await ctx.tick()
 
-    @trustyavatar.command()
+    @trustyavatarset.command()
     async def status(self, ctx):
         """
             Toggle status automatic changing
@@ -109,7 +239,7 @@ class TrustyAvatar(getattr(commands, "Cog", object)):
         await self.config.status.set(not is_override)
         await ctx.send("Status override set to " + str(not is_override))
 
-    @trustyavatar.command()
+    @trustyavatarset.command()
     async def avatar(self, ctx):
         """
             Toggle avatar automatic changing
@@ -118,7 +248,7 @@ class TrustyAvatar(getattr(commands, "Cog", object)):
         await self.config.avatar.set(not is_override)
         await ctx.send("Avatar override set to " + str(not is_override))
 
-    @trustyavatar.command()
+    @trustyavatarset.command()
     async def streaming(self, ctx):
         """
             Toggle owner streaming sync
