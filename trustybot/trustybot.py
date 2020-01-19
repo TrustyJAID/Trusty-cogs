813869fc4dddcd0849384e6c0c82d90952c3b03d
TrustyJAID <TrustyJAID@gmail.com>
2020-01-19 15:30:44 -0700

    [TrustyBot] 1.0.0 Remove beemovie command since emojis no longer worked

--- trustybot/trustybot.py
+++ trustybot/trustybot.py
@@ -10,13 +10,22 @@ class TrustyBot(commands.Cog):
         Generally for commands that don't fit anywhere else or are
         not meant to be used by anyone except TrustyBot
     """
+    __author__ = ["TrustyJAID"]
+    __version__ = "1.0.0"
 
     def __init__(self, bot):
         self.bot = bot
 
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """
+            Thanks Sinbad!
+        """
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nCog Version: {self.__version__}"
+
     @commands.command(hidden=True)
     @checks.is_owner()
-    async def trustyrules(self, ctx):
+    async def trustyrules(self, ctx: commands.Context):
         """
             Display rules for Trusty's testing server
         """
@@ -48,12 +57,12 @@ class TrustyBot(commands.Cog):
         await ctx.send(embed=em)
 
     @commands.command(hidden=True)
-    async def say(self, ctx, *, msg: str):
+    async def say(self, ctx: commands.Context, *, msg: str):
         """Say things as the bot"""
         await ctx.send(msg)
 
     @commands.command(hidden=True, aliases=["ss"])
-    async def silentsay(self, ctx, *, msg: str):
+    async def silentsay(self, ctx: commands.Context, *, msg: str):
         """Say things as the bot and deletes the command if it can"""
         if ctx.channel.permissions_for(ctx.guild).manage_messages:
             await ctx.message.delete()
@@ -61,7 +70,7 @@ class TrustyBot(commands.Cog):
 
     @commands.command(hidden=True, aliases=["hooksay"])
     @commands.bot_has_permissions(manage_webhooks=True)
-    async def websay(self, ctx, member: Optional[discord.Member], *, msg: str):
+    async def websay(self, ctx: commands.Context, member: Optional[discord.Member], *, msg: str):
         """
             Say things as another user
 
@@ -88,7 +97,7 @@ class TrustyBot(commands.Cog):
         await webhook.send(msg, username=member.display_name, avatar_url=avatar)
 
     @commands.command()
-    async def pingtime(self, ctx):
+    async def pingtime(self, ctx: commands.Context):
         """Ping pong."""
 
         # https://github.com/aikaterna/aikaterna-cogs/blob/v3/pingtime/pingtime.py
@@ -98,77 +107,24 @@ class TrustyBot(commands.Cog):
         await ctx.send(msg)
 
     @commands.command(aliases=["guildhelp", "serverhelp", "helpserver"])
-    async def helpguild(self, ctx):
+    async def helpguild(self, ctx: commands.Context):
         """
             Invites you to TrustyBot's playground for testing
+
+            https://discord.gg/wVVrqej
         """
         await ctx.send("https://discord.gg/wVVrqej")
 
     @commands.command()
-    @commands.cooldown(1, 3600, commands.BucketType.guild)
-    async def beemovie(self, ctx):
+    async def donate(self, ctx: commands.Context):
         """
-            Yes the actual bee movie as emojis
+            Donate to the development of TrustyBot!
 
-            I was able to upload them to my server before Discord
-            decreased the size limits on gif emojis
+            https://trustyjaid.com
         """
-        msg = (
-            "<a:bm1_1:394355466022551552>"
-            "<a:bm1_2:394355486625103872>"
-            "<a:bm1_3:394355526496026624>"
-            "<a:bm1_4:394355551859113985>"
-            "<a:bm1_5:394355549581606912>"
-            "<a:bm1_6:394355542849617943>"
-            "<a:bm1_7:394355537925373952>"
-            "<a:bm1_8:394355511912300554>\n"
-            "<a:bm2_1:394355541616361475>"
-            "<a:bm2_2:394355559719239690>"
-            "<a:bm2_3:394355587409772545>"
-            "<a:bm2_4:394355593567272960>"
-            "<a:bm2_5:394355578337624064>"
-            "<a:bm2_6:394355586067726336>"
-            "<a:bm2_7:394355558104432661>"
-            "<a:bm2_8:394355539716472832>\n"
-            "<a:bm3_1:394355552626409473>"
-            "<a:bm3_2:394355572381843459>"
-            "<a:bm3_3:394355594955456532>"
-            "<a:bm3_4:394355578253737984>"
-            "<a:bm3_5:394355579096793098>"
-            "<a:bm3_6:394355586411528192>"
-            "<a:bm3_7:394355565788397568>"
-            "<a:bm3_8:394355551556861993>\n"
-            "<a:bm4_1:394355538181488640>"
-            "<a:bm4_2:394355548944072705>"
-            "<a:bm4_3:394355568669884426>"
-            "<a:bm4_4:394355564504809485>"
-            "<a:bm4_5:394355567843606528>"
-            "<a:bm4_6:394355577758679040>"
-            "<a:bm4_7:394355552655900672>"
-            "<a:bm4_8:394355527867564032>"
-        )
-        if ctx.channel.permissions_for(ctx.me).embed_links:
-            em = discord.Embed(title="The Entire Bee Movie", description=msg)
-            await ctx.send(embed=em)
-        else:
-            await ctx.send(msg)
-
-    @commands.command()
-    async def donate(self, ctx):
-        """Donate to the development of TrustyBot!"""
         msg = (
             "Help support me and my work on TrustyBot "
             "by buying my album or donating, details "
             "on my website at the bottom :smile: https://trustyjaid.com/"
-        )
-        await ctx.send(msg)
-
-    @commands.command(hidden=False)
-    async def halp(self, ctx, user: discord.Member = ""):
-        """How to ask for help!"""
-        msg = (
-            f"{user} please type `{ctx.prefix}help` to be PM'd all my commands! "
-            ":smile: or type `;guildhelp` to get an invite and "
-            "I can help you personally."
         )
         await ctx.send(msg)
