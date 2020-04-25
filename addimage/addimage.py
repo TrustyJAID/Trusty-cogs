3375c7748a9866f9a4c4e3ee305324c2e9283b23
flare - Jamie <flare2399@gmail.com>
2020-04-25 19:10:51 +0100

    [addimage] mod.settings -> mod.config

--- addimage/addimage.py
+++ addimage/addimage.py
@@ -171,8 +171,12 @@ class AddImage(commands.Cog):
         )
         if surpass_ignore:
             return True
-        guild_ignored = await mod.settings.guild(guild).ignored()
-        chann_ignored = await mod.settings.channel(channel).ignored()
+        if hasattr(mod, "settings"):
+            guild_ignored = await mod.settings.guild(guild).ignored()
+            chann_ignored = await mod.settings.channel(channel).ignored()
+        else:
+            guild_ignored = await mod.config.guild(guild).ignored()
+            chann_ignored = await mod.config.channel(channel).ignored()
         return not (guild_ignored or chann_ignored and not perms.manage_channels)
 
     @commands.Cog.listener()
