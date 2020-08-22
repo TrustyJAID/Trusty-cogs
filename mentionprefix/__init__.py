3a1f1bd27836baa2c4be74714b3a21107d492922
Draper <27962761+Drapersniper@users.noreply.github.com>
2020-08-22 23:07:10 +0100

    meh (#148)

--- mentionprefix/__init__.py
+++ mentionprefix/__init__.py
@@ -1,10 +1,13 @@
+import asyncio
+
 from .mentionprefix import MentionPrefix
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
 )
 
+
 async def setup(bot):
     cog = MentionPrefix(bot)
     bot.add_cog(cog)
-    await cog.initialize()
+    asyncio.create_task(cog.initialize())
