22ace748016b3019201a3c8855aadfbed6d8c10d
TrustyJAID <TrustyJAID@gmail.com>
2020-09-27 12:03:09 -0600

    how tf

---
+++ spotify/__init__.py
@@ -0,0 +1,14 @@
+import json
+
+from pathlib import Path
+
+from .spotify import Spotify
+
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+def setup(bot):
+    cog = Spotify(bot)
+    bot.add_cog(cog)
