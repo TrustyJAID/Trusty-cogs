5c8b4aa48955bbbb8a559fe84b12ab01917c34bd
PhenoM4n4n <phenom4n4n@gmail.com>
2021-06-07 23:14:38 -0700

    add emoji validator

--- starboard/converters.py
+++ starboard/converters.py
@@ -1,5 +1,6 @@
-from discord.ext.commands.converter import Converter
-from discord.ext.commands.errors import BadArgument
+from typing import Union
+
+import discord
 from redbot.core import commands
 from redbot.core.i18n import Translator
 
@@ -8,14 +9,28 @@ from .starboard_entry import StarboardEntry
 _ = Translator("Starboard", __file__)
 
 
-class StarboardExists(Converter):
+class StarboardExists(commands.Converter):
     async def convert(self, ctx: commands.Context, argument: str) -> StarboardEntry:
         cog = ctx.cog
         guild = ctx.guild
         if guild.id not in cog.starboards:
-            raise BadArgument(_("There are no starboards setup on this server!"))
+            raise commands.BadArgument(_("There are no starboards setup on this server!"))
         try:
             starboard = cog.starboards[guild.id][argument.lower()]
         except KeyError:
-            raise BadArgument(_("There is no starboard named {name}").format(name=argument))
+            raise commands.BadArgument(_("There is no starboard named {name}").format(name=argument))
         return starboard
+
+
+class RealEmoji(commands.EmojiConverter):
+    async def convert(self, ctx: commands.Context, argument: str) -> Union[discord.Emoji, str]:
+        try:
+            emoji = await super().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                await ctx.message.add_reaction(argument)
+            except discord.HTTPException:
+                raise commands.EmojiNotFound(argument)
+            else:
+                emoji = argument
+        return emoji
