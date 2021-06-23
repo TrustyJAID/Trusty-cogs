b61db5bbea7e55c8d7fa9716d5d0f30beff30f43
TrustyJAID <TrustyJAID@gmail.com>
2021-06-23 15:02:28 -0600

    [Twitch] 1.4.0 Buttons!

--- twitch/twitch.py
+++ twitch/twitch.py
@@ -22,7 +22,7 @@ class Twitch(TwitchAPI, commands.Cog):
     """
 
     __author__ = ["TrustyJAID"]
-    __version__ = "1.3.7"
+    __version__ = "1.4.0"
 
     def __init__(self, bot):
         self.bot = bot
