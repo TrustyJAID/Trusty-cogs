569c54b5b5c8b3698f9ad303d965a5e27e43dfbc
TrustyJAID <TrustyJAID@gmail.com>
2022-03-18 18:33:12 -0600

    [RoleTools] check correct config variable

--- roletools/__init__.py
+++ roletools/__init__.py
@@ -10,5 +10,5 @@ with open(Path(__file__).parent / "info.json") as fp:
 async def setup(bot):
     cog = RoleTools(bot)
     await bot.add_cog(cog)
-    if not await cog.config.slash_enabled():
+    if not await cog.config.enable_slash():
         bot.tree.remove_command("role-tools")
