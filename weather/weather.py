85b7bb0e4df98d1ba2b505aea5d28e9f774a311d
TrustyJAID <TrustyJAID@gmail.com>
2022-04-30 14:32:56 -0600

    [Weather] Add hybrid commands

--- weather/weather.py
+++ weather/weather.py
@@ -1,34 +1,17 @@
-import datetime
 from typing import Literal, Optional
 from urllib.parse import urlencode
 
 import aiohttp
 import discord
-from discord.ext.commands.converter import Converter
-from discord.ext.commands.errors import BadArgument
-from redbot.core import Config, checks, commands
-from redbot.core.i18n import Translator, cog_i18n
+from redbot.core import Config, checks, commands, i18n
 
-_ = Translator("Weather", __file__)
+_ = i18n.Translator("Weather", __file__)
 
 
-class UnitConverter(Converter):
-    async def convert(self, ctx: commands.Context, argument: str) -> Optional[str]:
-        new_units = None
-        if argument.lower() in ["f", "imperial", "mph"]:
-            new_units = "imperial"
-        elif argument.lower() in ["c", "metric", "kph"]:
-            new_units = "metric"
-        elif argument.lower() in ["k", "kelvin"]:
-            new_units = "kelvin"
-        elif argument.lower() in ["clear", "none"]:
-            new_units = None
-        else:
-            raise BadArgument(_("`{units}` is not a vaild option!").format(units=argument))
-        return new_units
+Units = Literal["imperial", "metric", "kelvin"]
 
 
-@cog_i18n(_)
+@i18n.cog_i18n(_)
 class Weather(commands.Cog):
     """Get weather data from https://openweathermap.org"""
 
@@ -66,8 +49,7 @@ class Weather(commands.Cog):
         """
         await self.config.user_from_id(user_id).clear()
 
-    @commands.group(name="weather", aliases=["we"], invoke_without_command=True)
-    @commands.bot_has_permissions(embed_links=True)
+    @commands.hybrid_group(name="weather", aliases=["we"], fallback="location")
     async def weather(self, ctx: commands.Context, *, location: str) -> None:
         """
         Display weather in a given location
@@ -75,7 +57,8 @@ class Weather(commands.Cog):
         `location` must take the form of `city, Country Code`
         example: `[p]weather New York,US`
         """
-        await ctx.trigger_typing()
+        if not ctx.interaction:
+            await ctx.trigger_typing()
         await self.get_weather(ctx, location=location)
 
     @weather.command(name="zip")
@@ -87,7 +70,8 @@ class Weather(commands.Cog):
         `zipcode` must be a valid ZIP code or `ZIP code, Country Code` (assumes US otherwise)
         example: `[p]weather zip 20500`
         """
-        await ctx.trigger_typing()
+        if not ctx.interaction:
+            await ctx.trigger_typing()
         await self.get_weather(ctx, zipcode=zipcode)
 
     @weather.command(name="cityid")
@@ -100,10 +84,11 @@ class Weather(commands.Cog):
         (get list here: <https://bulk.openweathermap.org/sample/city.list.json.gz>)
         example: `[p]weather cityid 2172797`
         """
-        await ctx.trigger_typing()
+        if not ctx.interaction:
+            await ctx.trigger_typing()
         await self.get_weather(ctx, cityid=cityid)
 
-    @weather.command(name="co", aliases=["coords", "coordinates"])
+    @weather.command(name="coords", aliases=["co", "coordinates"])
     @commands.bot_has_permissions(embed_links=True)
     async def weather_by_coordinates(self, ctx: commands.Context, lat: float, lon: float) -> None:
         """
@@ -113,10 +98,11 @@ class Weather(commands.Cog):
         geographic coordinates specified by latitude (north-south) and longitude (east-west).
         example: `[p]weather coordinates 35 139`
         """
-        await ctx.trigger_typing()
+        if not ctx.interaction:
+            await ctx.trigger_typing()
         await self.get_weather(ctx, lat=lat, lon=lon)
 
-    @commands.group(name="weatherset")
+    @weather.group(name="set")
     async def weather_set(self, ctx: commands.Context) -> None:
         """Set user or guild default units"""
         pass
@@ -124,7 +110,7 @@ class Weather(commands.Cog):
     @weather_set.command(name="guild", aliases=["server"])
     @checks.mod_or_permissions(manage_messages=True)
     @commands.guild_only()
-    async def set_guild(self, ctx: commands.Context, units: UnitConverter) -> None:
+    async def set_guild(self, ctx: commands.Context, units: Units) -> None:
         """
         Sets the guild default weather units
 
@@ -136,7 +122,7 @@ class Weather(commands.Cog):
 
     @weather_set.command(name="bot")
     @checks.mod_or_permissions(manage_messages=True)
-    async def set_bot(self, ctx: commands.Context, units: UnitConverter) -> None:
+    async def set_bot(self, ctx: commands.Context, units: Units) -> None:
         """
         Sets the bots default weather units
 
@@ -146,7 +132,7 @@ class Weather(commands.Cog):
         await ctx.send(_("Bots default units set to {units}").format(units=str(units)))
 
     @weather_set.command(name="user")
-    async def set_user(self, ctx: commands.Context, units: UnitConverter) -> None:
+    async def set_user(self, ctx: commands.Context, units: Units) -> None:
         """
         Sets the user default weather units
 
