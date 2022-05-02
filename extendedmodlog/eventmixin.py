a741eec6825f78a9e7c81eb3ef8e021efba1f5f5
TrustyJAID <TrustyJAID@gmail.com>
2022-05-01 21:13:47 -0600

    [ExtendedModLog] Add logging support for hybrid commands. This may need more tweaking in the future.

--- extendedmodlog/eventmixin.py
+++ extendedmodlog/eventmixin.py
@@ -181,19 +181,15 @@ class EventMixin:
         can_x = _("**See:** {can_see}\n**Run:** {can_run}").format(
             can_run=can_run, can_see=can_see
         )
-        parent = " ".join(c for c in ctx.invoked_parents)
+        logger.debug(f"{ctx.command.qualified_name}")
+        parent = ""
         if ctx.interaction:
-            parents = []
-            com = ctx.command
-            while com.parent is not None:
-                parents.append(com.parent.name)
-                com = com.parent
-            parent = " ".join(c for c in parents)
+            parent = ctx.command.app_command.qualified_name
         kwargs = " ".join(str(v) for v in ctx.kwargs.values() if v is not None)
         args = " ".join(
             str(v) for v in ctx.args if v and not isinstance(v, (commands.Cog, commands.Context))
         )
-        com_str = f"{ctx.clean_prefix}{parent} {ctx.invoked_with} {args} {kwargs}"
+        com_str = f"{ctx.clean_prefix}{parent}{ctx.invoked_with} {args} {kwargs}"
         try:
             privs = ctx.command.requires.privilege_level
             user_perms = ctx.command.requires.user_perms
