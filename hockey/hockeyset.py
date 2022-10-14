90d6f2b2459819df4da5676851fd09fe9dd30a44
TrustyJAID <TrustyJAID@gmail.com>
2022-10-13 21:47:28 -0600

    [Hockey] Fix Slash command registration

--- hockey/hockeyset.py
+++ hockey/hockeyset.py
@@ -120,7 +120,7 @@ class HockeySetCommands(MixinMeta):
         verb = _("enabled") if not current else _("disabled")
         await ctx.send(_("Slash commands are {verb}.").format(verb=verb))
         if not current:
-            self.bot.tree.add_command(self)
+            self.bot.tree.add_command(self.hockey_commands.app_command)
         else:
             self.bot.tree.remove_command("hockey")
 
